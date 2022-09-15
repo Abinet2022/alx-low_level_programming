f32107e3fa05d8840b226c306873123e953cdc46
Abinet12 <abinetmeri2020@gmail.com>
2022-09-14 23:04:44 -0700

    puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
-#!/bin/bash
-#include <studio.h>                                                                                                          
-int main(void)                                                                                                               
-{                                                                                                                            
-puts("\"Programming is like building a multilingual puzzle");                                                                
-return(0);                                                                                                                   
-}   
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingua
+l puzzle");
+return(0);
+}
