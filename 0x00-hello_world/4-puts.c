af6b63fba01395e8604fa3218dee07a2d90fe6bc
Abinet12 <abinetmeri2020@gmail.com>
2022-09-14 23:24:12 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-/**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
-int main(void)
-{
-puts("\"Programming is like building a multilingua
-l puzzle");
-return(0);
-}
+#include <stdio.h>
+/**                                                              
+* main - print the string in the put function                    
+*                                                                
+* Description: using the main function                           
+* this program prints "Programming is like building a multilingua
+* Return: 0                                                      
+*/                                                               
+int main(void)                                                   
+{                                                                
+puts("\"Programming is like building a multilingua               
+l puzzle");                                                      
+return(0);                                                       
+}  
