d7124ec67dcbf841e0546a2c4cd3e8552e6d90c9
Abinet12 <abinetmeri2020@gmail.com>
2022-09-16 02:20:06 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer if integar ispositive or negative
+ * return: zero
+ */
+
+int main(void)
+
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
+:wq
+
